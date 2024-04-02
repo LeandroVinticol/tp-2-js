50913baa2b7764032f7c6ae115555f2e311cfe66
Leandro Vinticol <leovinticol@gmail.com>
2024-04-02 17:46:07 -0300

    ejercicio 2

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,4 +1,4 @@
-//Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+//1. Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
 
 const edad = parseInt(prompt("Ingresá tu edad"));
 
