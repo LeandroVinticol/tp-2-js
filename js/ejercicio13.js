66332aab9e3ea6515907ca805843f9fc329d77b3
Leandro Vinticol <leovinticol@gmail.com>
2024-04-03 19:12:12 -0300

    ejercicio 13

---
+++ js/ejercicio13.js
@@ -0,0 +1,5 @@
+//13. Realiza un script que pida un texto y lo muestre en mayúsculas.
+texto = prompt("Ingresar texto");
+textoMay = texto.toUpperCase();
+
+console.log(`Tu texto en mayuscula es ${textoMay}`);
